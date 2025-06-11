f2d5234da73da27b27e0447646c12362d3608e64
WoaGamer <wanderson12adv@hotmail.com>
2025-06-10 22:42:52 -0400

    Add capture progress HUD

---
+++ KOTH/Scripts/5_Mission/KOTH_ProgressBar.c
@@ -0,0 +1,36 @@
+class KOTH_ProgressBar {
+    protected Widget m_Root;
+    protected ProgressBarWidget m_Bar;
+    protected TextWidget m_Text;
+    protected bool m_Visible;
+
+    void KOTH_ProgressBar() {
+        if (!GetGame()) return;
+        m_Root = GetGame().GetWorkspace().CreateWidgets("KOTH/gui/layouts/koth_progress.layout");
+        m_Bar = ProgressBarWidget.Cast(m_Root.FindAnyWidget("ProgressBar"));
+        m_Text = TextWidget.Cast(m_Root.FindAnyWidget("ProgressText"));
+        Hide();
+    }
+
+    void Show() {
+        if (m_Root) m_Root.Show(true);
+        m_Visible = true;
+    }
+
+    void Hide() {
+        if (m_Root) m_Root.Show(false);
+        m_Visible = false;
+    }
+
+    bool IsVisible() {
+        return m_Visible;
+    }
+
+    void SetProgress(float percent) {
+        if (!m_Bar || !m_Text) return;
+        if (percent < 0) percent = 0;
+        if (percent > 100) percent = 100;
+        m_Bar.SetCurrent(percent);
+        m_Text.SetText(string.Format("%1%%", Math.Round(percent).ToString()));
+    }
+}
