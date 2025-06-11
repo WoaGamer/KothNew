cb1ddbfc6a81c5a64d26e32e4343ef8e0edbe1ba
WoaGamer <wanderson12adv@hotmail.com>
2025-06-10 22:53:45 -0400

    Add BasicMap stub to prevent compilation error

--- KOTH/Scripts/3_Game/3rdParty/BasicMap.c
+++ KOTH/Scripts/3_Game/3rdParty/BasicMap.c
@@ -1,9 +1,37 @@
-# ifdef BASICMAP
-modded class BasicMapController extends Managed {
-    override void Init() {
-        if (IsInit) SetMarkers("KOTH", new array < autoptr BasicMapMarker > );
+#ifdef BASICMAP
 
-        super.Init();
+/**
+ * In some deployments the BasicMap mod might not be present. When it is
+ * missing the original <code>BasicMapController</code> class is undefined
+ * causing a compilation error.  To avoid this we provide a very small stub
+ * implementation that satisfies the compiler.  The methods simply perform no
+ * action which means the map marker feature is effectively disabled when the
+ * real module is not available.
+ */
+class BasicMapMarker {}
+
+class BasicMapController extends Managed
+{
+    bool IsInit;
+
+    void SetMarkers(string group, array<autoptr BasicMapMarker> markers) {}
+    void SetMarkersRemote(string group, array<ref BasicMapMarker> markers, PlayerIdentity target = NULL) {}
+    void RequestGroupUpdate(string group) {}
+
+    void Init()
+    {
+        if (IsInit)
+            SetMarkers("KOTH", new array<autoptr BasicMapMarker>);
     }
 }
-# endif
+
+static ref BasicMapController g_BasicMap;
+
+BasicMapController BasicMap()
+{
+    if (!g_BasicMap)
+        g_BasicMap = new BasicMapController();
+    return g_BasicMap;
+}
+
+#endif
